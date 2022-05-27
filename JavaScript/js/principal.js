c41307927ce336dd974b979d2c1ab9a34bd112fa
joaogami <ti.gabrielmiranda@gmail.com>
2022-05-27 05:19:28 -0300

    Mais um capítulo de JS

--- JavaScript/js/principal.js
+++ JavaScript/js/principal.js
@@ -3,36 +3,68 @@ titulo.textContent = "Aparecida Nutricionista";
 
 var pacientes = document.querySelectorAll(".paciente");
 
-for(var i = 0 ; i < pacientes.length ; i++){
-console.log(i)
-console.log(pacientes[i]);
-var paciente = pacientes[i];
-var tdPeso = paciente.querySelector(".info-peso");
-var tdAltura = paciente.querySelector(".info-altura");
-var tdImc = paciente.querySelector(".info-imc");
+    for(var i = 0 ; i < pacientes.length ; i++){
 
-var altura = tdAltura.textContent;
-var peso = tdPeso.textContent;
-var alturaEhValida = true;
-var pesoEhValido = true;
+    var paciente = pacientes[i];
+    var tdPeso = paciente.querySelector(".info-peso");
+    var tdAltura = paciente.querySelector(".info-altura");
+    var tdImc = paciente.querySelector(".info-imc");
 
-if(peso <= 0 || peso >= 1000){
-    console.log("Peso inválido");
-    tdImc.textContent = "Peso inválido!";
-    pesoEhValido = false;
-    paciente.classList.add("paciente-invalido");
-}
+    var altura = tdAltura.textContent;
+    var peso = tdPeso.textContent;
+    var alturaEhValida = true;
+    var pesoEhValido = true;
 
-if(altura <= 0 || altura >= 3.0){
-    console.log("Altura inválida");
-    tdImc.textContent = "Altura inválida!";
-    alturaEhValida = false;
-    paciente.classList.add("paciente-invalido");
-}
+        if(peso <= 0 || peso >= 1000){
+            console.log("Peso inválido");
+            tdImc.textContent = "Peso inválido!";
+            pesoEhValido = false;
+            paciente.classList.add("paciente-invalido");
+        }
 
-if(pesoEhValido && alturaEhValida){
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc.toFixed(2);
-}
-}
-console.log("Acabou");
+        if(altura <= 0 || altura >= 3.0){
+            console.log("Altura inválida");
+            tdImc.textContent = "Altura inválida!";
+            alturaEhValida = false;
+            paciente.classList.add("paciente-invalido");
+        }
+
+        if(pesoEhValido && alturaEhValida){
+            var imc = peso / (altura * altura);
+
+            tdImc.textContent = imc.toFixed(2);
+        }
+    }
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+botaoAdicionar.addEventListener("click", function(){
+    event.preventDefault();
+    
+    var form = document.querySelector("#form-adiciona");
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+    
+    var pacienteTr = document.createElement("tr");
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContet = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+})
--- JavaScript/readme.md
+++ JavaScript/readme.md
@@ -10,3 +10,27 @@ O comando <code>document.querySelector("")</code> busca dados na classe,id ou ob
 O comando tdpeso busca dentro das tags já buscadas em paciente.
 
 Por fim o variável.textcontent remove as tags do texto exibindo apenas o conteúdo.
+
+~~~javascript
+    variavel.toFixed(numero de casas decimais);
+~~~
+A Função <code>toFixed()</code> limita a quantidade de casas decimais que serão exibidas.
+
+~~~javascript
+    variavel.addEventListener("click", função);
+    //chama uma função já declarada no evento de click
+    variavel.addEventListener("click", função(){
+    //cria a função para este evento de click
+    });
+~~~
+A Função <code>addEventListener</code> "ouve" o evento do usuário na página.
+
+~~~javascript
+    event.preventDefault();
+~~~
+A função <code>event.preventDefault()</code> previne o comportamento padrão da página.
+
+~~~javascript
+    variavelPai.appendChild(variavelFilho);
+~~~
+A função <code>appendChild</code> permite colocar um elemento 'filho'  dentro de outro elemento 'pai', como um <code><td></code> dentro de uma <code><tr></code>
